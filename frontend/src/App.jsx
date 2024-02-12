b0d2f9f2ad40f4e5660fa4e3660fcf299281a57e
Brandon Kaminski <140101047+bkaminski126@users.noreply.github.com>
2024-02-12 14:24:55 -0500

    settings update remove guesses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -171,6 +171,7 @@ function App() {
                   setId(randomId(minPop));
                   setShowMap(false);
                   setShowSettings(false);
+                  setGuesses([]);
                 }}
                 className="px-4 py-2 text-lg text-white bg-blue-700 rounded-lg hover:bg-blue-500"
               >
